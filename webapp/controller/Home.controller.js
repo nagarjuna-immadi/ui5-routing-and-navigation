7efeeda1d71b83becc4498f56f6a58a578882797
Nagarjuna Immadi <nagarjuna.immadi@real-digital.de>
2026-01-11 08:40:23 +0100

    Implement employee overview navigation and views with sorting and searching functionality

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -12,6 +12,9 @@ sap.ui.define([
 		},
 		onNavToEmployees : function (){
 			this.getRouter().navTo("employeeList");
+		},
+		onNavToEmployeeOverview : function ()  {
+			this.getRouter().navTo("employeeOverview");
 		}
    });
 
